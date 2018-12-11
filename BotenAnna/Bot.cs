7ce45a23f3e3a5f8d78a9b13a06d6943c4190336
karegren <karegren@student.chalmers.se>
2018-12-11 11:00:47 +0100

    Basic bot that works with emulator added

---
+++ BotenAnna/Bot.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.AI.QnA;
+using Microsoft.Bot.Schema;
+
+namespace BotenAnna
+{
+    public class Bot : IBot
+    {
+        public Bot(QnAMaker qnAMaker)
+        {
+            QnAMaker = qnAMaker;
+        }
+
+        public QnAMaker QnAMaker { get; }
+        public async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            if (!turnContext.Responded && turnContext.Activity.Type == ActivityTypes.Message)
+            {
+                var result = await QnAMaker.GetAnswersAsync(turnContext);
+
+                if (result != null && result.Length > 0)
+                {
+                    await turnContext.SendActivityAsync(result[0].Answer, cancellationToken: cancellationToken);
+                }
+                else
+                {
+                    var msg = @"Sorry, I don't have an answer to that";
+
+                    await turnContext.SendActivityAsync(msg, cancellationToken: cancellationToken);
+                }
+            }
+        }
+    }
+}
---
+++ BotenAnna/Startup.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Bot.Builder.AI.QnA;
+using Microsoft.Bot.Builder.BotFramework;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BotenAnna
+{
+    public class Startup
+    {
+
+ 
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
+        {
+            Configuration = configuration;
+            Env = env;
+            /*Insert Knowledge ID*/KnowledgeID = "";
+            /*Insert Host Adress*/HostAdress = "";
+            /*Insert end point key*/endPoint = "";
+
+        }
+        public string KnowledgeID { get; }
+        public string HostAdress { get; }
+        public string endPoint { get; }
+
+        public IConfiguration Configuration { get; }
+        public IHostingEnvironment Env { get; }
+
+        
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var configurationBuilder = new ConfigurationBuilder()
+                .SetBasePath(Env.ContentRootPath)
+                .AddJsonFile("appsettings.json");
+
+            var configuration = configurationBuilder
+                .Build();
+
+            services.AddBot<Bot>((options) => {
+                options.CredentialProvider = new ConfigurationCredentialProvider(configuration);
+            });
+
+            services.AddSingleton(sp =>
+            {
+                var qnaService = new QnAMaker(new QnAMakerEndpoint()
+                {
+  
+                    KnowledgeBaseId = KnowledgeID,
+                    Host = HostAdress,
+                    EndpointKey = endPoint
+                });
+
+                return qnaService;
+            });
+
+            services.AddMvc();
+        }
+            
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+            app.UseBotFramework();
+        }
+    }
+}
